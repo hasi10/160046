d2cd48a81df87e2f1b1caf47ddccd1e2ba7ece9a
hasi10 <hasimoto_shota@yse-c.net>
2016-12-13 15:13:22 +0900

    Aloha.java added

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,11 +1,13 @@
 public class Aloha {
 	public static void main(String[] args) {
-		String msg = 
+		String msg = args[0];
+		String msg2 = args[1];
+
 		for(int i = 0; i < 20;  i++){
 			if(i%3 == 0){
-		System.out.println("アロハ!");
+		System.out.println( msg );
 	} else {
-		System.out,println("ALOHA!");
+		System.out,println( msg2 );
 			}
 		}
 	}
